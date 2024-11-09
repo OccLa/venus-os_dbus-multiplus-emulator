1d40d7889e2b8c0d838d5a3a41c012fb19682151
OccLa <50462687+OccLa@users.noreply.github.com>
2024-11-09 04:19:58 -0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,6 +2,7 @@
 
 driver_path="/data/etc"
 driver_name="dbus-multiplus-emulator"
+repo_owner="occla"
 
 echo ""
 echo ""
@@ -10,10 +11,10 @@ echo ""
 echo -n "Fetch current version numbers..."
 
 # latest release
-latest_release_stable=$(curl -s https://api.github.com/repos/mr-manuel/venus-os_${driver_name}/releases/latest | grep "tag_name" | cut -d : -f 2,3 | tr -d "\ " | tr -d \" | tr -d \,)
+latest_release_stable=$(curl -s https://api.github.com/repos/$(repo_owner)/venus-os_${driver_name}/releases/latest | grep "tag_name" | cut -d : -f 2,3 | tr -d "\ " | tr -d \" | tr -d \,)
 
 # nightly build
-latest_release_nightly=$(curl -s https://raw.githubusercontent.com/mr-manuel/venus-os_${driver_name}/master/${driver_name}/${driver_name}.py | grep HardwareVersion | awk -F'"' '{print $4}')
+latest_release_nightly=$(curl -s https://raw.githubusercontent.com/$(repo_owner)/venus-os_${driver_name}/master/${driver_name}/${driver_name}.py | grep HardwareVersion | awk -F'"' '{print $4}')
 
 
 echo
@@ -68,13 +69,13 @@ echo "Downloading driver..."
 ## latest release
 if [ "$version" = "latest release \"$latest_release_stable\"" ]; then
     # download latest release
-    url=$(curl -s https://api.github.com/repos/mr-manuel/venus-os_${driver_name}/releases/latest | grep "zipball_url" | sed -n 's/.*"zipball_url": "\([^"]*\)".*/\1/p')
+    url=$(curl -s https://api.github.com/repos/$(repo_owner)/venus-os_${driver_name}/releases/latest | grep "zipball_url" | sed -n 's/.*"zipball_url": "\([^"]*\)".*/\1/p')
 fi
 
 ## nightly build
 if [ "$version" = "nightly build \"v$latest_release_nightly\"" ]; then
     # download nightly build
-    url="https://github.com/mr-manuel/venus-os_${driver_name}/archive/refs/heads/master.zip"
+    url="https://github.com/$(repo_owner)/venus-os_${driver_name}/archive/refs/heads/master.zip"
 fi
 
 echo "Downloading from: $url"
